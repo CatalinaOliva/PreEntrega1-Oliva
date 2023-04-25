aca0c6b43d593f7ce6f5def873e4def49a179665
Catalina Oliva <catyoliva2014@gmail.com>
2023-04-25 15:39:38 -0300

    Agrego Correcciones

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
-let nombre = prompt("Ingrese su Nombre");
-datos (nombre, "nombre")
+const MAX_NOTA = 10;
 
+let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
 let materia = prompt("Ingrese el nombre de la Materia");
-let primerParcial = parseFloat(prompt("Ingrese la nota de su primer Parcial"));
-notas (primerParcial, "primer");
-let segundoParcial = parseFloat(prompt("Ingrese la nota de su segundo parcial"));
-notas (segundoParcial, "segundo")
-let tercerParcial = parseFloat(prompt("Ingrese la nota de su tercer parcial"));
-notas (tercerParcial, "tercer")
+
+let primerParcial = validarNota("primer");
+let segundoParcial = validarNota("segundo");
+let tercerParcial = validarNota("tercer");
 
 let resultados = (primerParcial + segundoParcial + tercerParcial) / 3;
 let sumas = primerParcial + segundoParcial + tercerParcial;
 
 if (sumas < 30 && sumas > 24) {
-    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Felicitaciones, estas aprobado`);
+    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Felicitaciones, estás aprobado`);
 } else if (sumas < 24 && sumas > 14) {
-    let proximaNota = 24 - (primerParcial + segundoParcial + tercerParcial);
-    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Para estar aprobado, en tu próximo parcial deberas obtener como mínimo un ${proximaNota}`);
-} else if(sumas < 14) {
-    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Lamentablemente, estas desaprobado :( `);
+    let proximaNota = 24 - sumas;
+    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Para estar aprobado, en tu próximo parcial deberás obtener como mínimo un ${proximaNota}`);
+} else if (sumas < 14) {
+    alert(`Hola, ${nombre} ${apellido}, tu promedio general actual en ${materia} es de ${resultados}. Lamentablemente, estás desaprobado :(`);
 }
 
-function datos (b, c) {
-    while (b === Number | b === "") {
-        alert(`${b} No es un nombre valido!`);
-        nombre = prompt(`"Ingrese su ${c}`);
-    }
-}
-
-function notas(x, a) {
-    while (x > 10 | x < 1 | isNaN(x)) {
-        alert("El valor ingresado debe ser del 1 al 10.");
-        primerParcial = parseFloat(prompt(`Ingrese la nota de su ${a} Parcial`));
-        return;
-    }
+function validarNota(numParcial) {
+    let nota;
+    do {
+        nota = parseFloat(prompt(`Ingrese la nota de su ${numParcial} parcial`));
+        if (isNaN(nota) || nota < 1 || nota > MAX_NOTA) {
+            alert(`La nota ingresada debe ser un número entre 1 y ${MAX_NOTA}`);
+        }
+    } while (isNaN(nota) || nota < 1 || nota > MAX_NOTA);
+    return nota;
 }
