ec124ba078d9a0d1905f5f0a5720bfe482e38521
Catalina Oliva <catyoliva2014@gmail.com>
2023-04-24 22:49:32 -0300

    Agrego Funcion

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let nombre = prompt("Ingrese su Nombre");
 
-// function datos (x, ) {
+// function datos (x) {
 //     while (x === "string") {
 //         alert(`${x} No es un nombre valido!`);
 //         nombre = prompt("Ingrese su Nombre");
@@ -12,36 +12,26 @@ let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
 let materia = prompt("Ingrese el nombre de la Materia");
 let primerParcial = parseFloat(prompt("Ingrese la nota de su primer Parcial"));
+let segundoParcial = parseFloat(prompt("Ingrese la nota de su segundo parcial"));
+let tercerParcial = parseFloat(prompt("Ingrese la nota de su tercer parcial"));
 
+
+notas (primerParcial, "primer");
+notas (segundoParcial, "segundo")
+notas (tercerParcial, "tercer")
 
 function notas(x, a) {
     while (x > 10 | x < 1 | isNaN(x)) {
         alert("El valor ingresado debe ser del 1 al 10.");
         primerParcial = parseFloat(prompt(`Ingrese la nota de su ${a} Parcial`));
+        return;
     }
 }
-
-notas (primerParcial, "primer");
-
 
 // while (primerParcial > 10 | primerParcial < 1 | isNaN(primerParcial)) {
 //     alert("El valor ingresado debe ser del 1 al 10.");
 //     primerParcial = parseFloat(prompt("Ingrese la nota de su primer Parcial"));
 // }
-
-let segundoParcial = parseFloat(prompt("Ingrese la nota de su segundo parcial"));
-
-while (segundoParcial > 10 | segundoParcial < 1 | isNaN(segundoParcial)) {
-    alert("El valor ingresado debe ser del 1 al 10.");
-    segundoParcial = parseFloat(prompt("Ingrese la nota de su segundo parcial"));
-}
-
-let tercerParcial = parseFloat(prompt("Ingrese la nota de su tercer parcial"));
-
-while (tercerParcial > 10 | tercerParcial < 1 | isNaN(tercerParcial)) {
-    alert("El valor ingresado debe ser del 1 al 10.");
-    tercerParcial = parseFloat(prompt("Ingrese la nota de su tercer Parcial"));
-}
 
 let resultados = (primerParcial + segundoParcial + tercerParcial) / 3;
 let sumas = primerParcial + segundoParcial + tercerParcial;
